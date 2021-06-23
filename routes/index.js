bd3cfe2b6dce5d5208c9d1eea1d030a03a0a75d9
sam-azevedo <samuel.azevedo@live.com>
2021-06-23 15:27:37 -0300

    add body-parser package express-validator msg

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
 const express = require('express');
+const { body, validationResult } = require('express-validator');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
     res.render('form', { title: 'Registration form' });
 });
+router.post('/',
+    [
+        body('name')
+            .isLength({ min: 1 })
+            .withMessage('Please enter a name'),
+        body('email')
+            .isLength({ min: 1 })
+            .withMessage('Please enter an valid email'),
+    ],
+    (req, res) => {
+        const errors = validationResult(req);
+        //console.log(req.body);
+
+        if (errors.isEmpty()) {
+            res.send('Thank you for your registration! ');
+        } else {
+            res.render('form', {
+                title: 'Registration form',
+                errors: errors.array(),
+                data: req.body,
+            });
+        }
+    });
 
 module.exports = router;
